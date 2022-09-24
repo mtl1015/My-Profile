da9db10bde244bb61014d2c420b56bd8f8a48175
mtl1015 <95431754+mtl1015@users.noreply.github.com>
2022-09-24 17:03:32 +0700

    making preview pug template and link others template
    2022-09-24

--- src/controller/detailController.js
+++ src/controller/detailController.js
@@ -1,6 +1,16 @@
-export const mainDetail = (req, res) => {};
+export const mainDetail = (req, res) => {
+  res.render("detail", { pageTitle: "Detail" });
+};
 
-export const codingDetail = (req, res) => {};
-export const civilDetail = (req, res) => {};
-export const travelDetail = (req, res) => {};
-export const teacherDetail = (req, res) => {};
+export const codingDetail = (req, res) => {
+  res.render("coding", { pageTitle: "Coding" });
+};
+export const civilDetail = (req, res) => {
+  res.render("Civil", { pageTitle: "Civil" });
+};
+export const travelDetail = (req, res) => {
+  res.render("travel", { pageTitle: "Travel" });
+};
+export const teacherDetail = (req, res) => {
+  res.render("teacher", { pageTitle: "Teacher" });
+};
--- src/controller/rootController.js
+++ src/controller/rootController.js
@@ -1,5 +1,23 @@
-export const previewPage = (req, res) => {};
+function getClock() {
+  const date = new Date();
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+  if (hours >= 12) {
+    hours = hours - 12;
+  } else {
+    hours;
+  }
+  const seconds = date.getSeconds();
+  const currentTime = `${hours}:${minutes}:${seconds}`;
+  return currentTime;
+}
+
+export const previewPage = (req, res) => {
+  getClock();
+  setInterval(getClock, 1000);
+  return res.render("preview", { pageTitle: "Hello!", getClock });
+};
 
 export const mainPage = (req, res) => {
-  res.send("Hello world!");
+  return res.render("main", { pageTitle: "Hello!" });
 };
